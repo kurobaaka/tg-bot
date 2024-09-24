8dca3f9fbb5389c7f236f7d75f13665a33400136
kurobaaka <psykhokidzxc@gmail.com>
2024-09-24 20:59:35 +0700

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const telegramApi = require('node-telegram-bot-api')
 const token = '7880249876:AAFOL6BKYH-QWQS7iwYNJuWTkM961es88u4'
-const bot = new telegramApi.Bot(token, {pooling: true})
+const bot = new telegramApi(token, {pooling: true})
 bot.on(event => 'message', listener => msg => {
     console.log(msg)
 })
