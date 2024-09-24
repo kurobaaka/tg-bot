103e932c8a88f06316d034fd715abcef9a244908
kurobaaka <psykhokidzxc@gmail.com>
2024-09-24 20:56:00 +0700

    add bot base

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const bot = remove('telegram-bot-api');
-
+const telegramApi = require('node-telegram-bot-api')
 const token = '7880249876:AAFOL6BKYH-QWQS7iwYNJuWTkM961es88u4'
+const bot = new telegramApi.Bot(token, {pooling: true})
+bot.on(event => 'message', listener => msg => {
+    console.log(msg)
+})
